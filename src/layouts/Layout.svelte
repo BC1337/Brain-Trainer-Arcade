5bf67ed8e88f000de6b92f4670121eb9c1252f41
BC1337 <brettcomrie@gmail.com>
2024-04-04 09:26:34 -0500

    added dark mode to the layout (all pages)

--- src/layouts/Layout.svelte
+++ src/layouts/Layout.svelte
@@ -1,7 +1,9 @@
 <script>
     import Navbar from "../lib/components/Navbar.svelte";
     import '../routes/styles.css'
+    import ThemeToggle from "../lib/components/ThemeToggle.svelte";
 </script>
 
 <Navbar />
 <slot />
+<ThemeToggle />
--- src/routes/games/+page.svelte
+++ src/routes/games/+page.svelte
@@ -1,41 +1,40 @@
 <script>
-    import Layout from "../../layouts/Layout.svelte";
-    import GameCard from "../../lib/components/Gamecard.svelte";
- // Array of game objects
- let games = [
-    {
-
-      gameName: 'Type Racer',
-      gameDescription: 'Put your typing skills to the test',
-      imageSrc: 'https://images.pexels.com/photos/373072/pexels-photo-373072.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-    },
-    {
-
-      gameName: 'Memory Trainer',
-      gameDescription: 'Flex your mental muscles to go the distance in this game.',
-      imageSrc: 'https://images.pexels.com/photos/577513/pexels-photo-577513.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-    },
-    {
-      gameName: 'Simon Says',
-      gameDescription: 'Test your short term memory with this classic',
-      imageSrc: '/simon-says.jpg'
-    },
-    {
-      gameName: 'Aim Trainer',
-      gameDescription: 'How about testing that hand eye coordination',
-      imageSrc: 'https://images.pexels.com/photos/416832/pexels-photo-416832.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-    },
-    {
-      gameName: 'Reaction Timer',
-      gameDescription: 'Wait for the light to turn from red to green and see how fast you click!',
-      imageSrc: 'https://images.pexels.com/photos/1727004/pexels-photo-1727004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-    },
-    {
-      gameName: 'Chimp test',
-      gameDescription: 'Are you a monkeys uncle...? The results will speak for themselves.',
-      imageSrc: 'https://images.pexels.com/photos/1123771/pexels-photo-1123771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-    },
-    // Add more game objects as needed
+  import Layout from "../../layouts/Layout.svelte";
+  import GameCard from "../../lib/components/Gamecard.svelte";
+  
+  // Array of game objects
+  let games = [
+      {
+          gameName: 'Type Racer',
+          gameDescription: 'Put your typing skills to the test',
+          imageSrc: 'https://images.pexels.com/photos/373072/pexels-photo-373072.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+      },
+      {
+          gameName: 'Memory Trainer',
+          gameDescription: 'Flex your mental muscles to go the distance in this game.',
+          imageSrc: 'https://images.pexels.com/photos/577513/pexels-photo-577513.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+      },
+      {
+          gameName: 'Simon Says',
+          gameDescription: 'Test your short term memory with this classic',
+          imageSrc: '/simon-says.jpg'
+      },
+      {
+          gameName: 'Aim Trainer',
+          gameDescription: 'How about testing that hand eye coordination',
+          imageSrc: 'https://images.pexels.com/photos/416832/pexels-photo-416832.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+      },
+      {
+          gameName: 'Reaction Timer',
+          gameDescription: 'Wait for the light to turn from red to green and see how fast you click!',
+          imageSrc: 'https://images.pexels.com/photos/1727004/pexels-photo-1727004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+      },
+      {
+          gameName: 'Chimp test',
+          gameDescription: 'Are you a monkey\'s uncle...? The results will speak for themselves.',
+          imageSrc: 'https://images.pexels.com/photos/1123771/pexels-photo-1123771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+      },
+      // Add more game objects as needed
   ];
 </script>
 
@@ -47,14 +46,22 @@
   <!-- Add more meta tags as needed -->
 </head>
 
-
 <Layout>
   <main aria-label="Games Hub">
-    <h1>Enjoy some brain games and continue to level up!</h1>
-    <GameCard games={games} />
-</main>
+      <h1><span class="highlight">Enjoy</span> some <span class="highlight">brain games</span> and continue to <span class="highlight">level up</span>!</h1>
+      <GameCard games={games} />
+  </main>
 </Layout>
 
 <style>
+  h1 {
+      margin-top: 50px;
+      display: flex;
+      justify-content: center;
+  }
 
+  .highlight {
+      color: #f0a500; /* Orange color */
+      padding: 0 5px; /* Add spacing */
+  }
 </style>
