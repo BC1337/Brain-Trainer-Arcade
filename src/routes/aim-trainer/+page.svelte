05c47d5ee072b152cf1a76ce9c32ecf03077f9e4
BC1337 <brettcomrie@gmail.com>
2024-06-04 21:44:44 -0500

    Feat: Fixed Canvas not resizing properly. removed stats for now, going to remake the end game menu to display stats soon

--- src/routes/aim-trainer/+page.svelte
+++ src/routes/aim-trainer/+page.svelte
@@ -1,15 +1,20 @@
 <script>
-  import { onMount } from "svelte";
+  import { onMount, onDestroy } from "svelte";
   import Layout from "../../layouts/Layout.svelte";
 
-  let playAreaWidth = 540;
-  let playAreaHeight = 540;
+  let playAreaWidth;
+  let playAreaHeight;
+  const minWidth = 365; // Minimum width for play area
+  const maxWidth = 1000; // Maximum width for play area
+  const screenHeightPercentage = 0.75; // Adjust the screen height percentage as needed
+  const minHeight = 0; // Minimum height for play area
+
   let gameStarted = false;
   let timer = 10;
   let successfulClicks = 0;
   let missedClicks = 0;
   let totalClicks = 0;
-  let clickTimes = [];
+  let accuracy = 0;
   let gameEnded = false;
   let lastClickTime = null;
   let targetX;
@@ -19,32 +24,57 @@
 
   let canvas;
 
-  onMount(() => {
-    canvas = document.getElementById('play-area');
+  const isBrowser = typeof window !== 'undefined';
 
+  onMount(() => {
+    if (isBrowser) {
+      canvas = document.getElementById('play-area');
+      resizeCanvas();
+      window.addEventListener('resize', resizeCanvas);
+    }
+    return () => {
+      if (isBrowser) {
+        window.removeEventListener('resize', resizeCanvas);
+      }
+    };
   });
 
+  onDestroy(() => {
+    if (isBrowser) {
+      window.removeEventListener('resize', resizeCanvas);
+    }
+  });
+
+  const resizeCanvas = () => {
+    playAreaWidth = Math.max(minWidth, Math.min(window.innerWidth * 0.9, maxWidth));
+    playAreaHeight = Math.max(window.innerHeight * screenHeightPercentage, minHeight);
+    if (canvas) {
+      canvas.width = playAreaWidth;
+      canvas.height = playAreaHeight;
+      if (gameStarted) {
+        const ctx = canvas.getContext('2d');
+        generateTarget(ctx);
+      }
+    }
+  };
+
   const startGame = () => {
-    hideModal(); // Hide the modal before starting the game
+    hideModal();
     gameStarted = true;
     gameEnded = false;
     successfulClicks = 0;
     missedClicks = 0;
     totalClicks = 0;
-    clickTimes = [];
-    timer = roundLength; // Set round length
+    timer = roundLength;
     lastClickTime = null;
     startTimer();
     setTimeout(() => {
-        const ctx = canvas.getContext('2d');
-        generateTarget(ctx);
+      const ctx = canvas.getContext('2d');
+      generateTarget(ctx);
     }, 100);
-    
-    // Hide game controls when the game starts
     const gameControlsContainer = document.querySelector('.game-controls-container');
-    gameControlsContainer.classList.remove('game-controls-visible');
     gameControlsContainer.classList.add('game-controls-hidden');
-};
+  };
 
   const hideModal = () => {
     const modal = document.getElementById('game-modal');
@@ -55,17 +85,12 @@
     if (gameStarted && ctx) {
       targetX = Math.random() * (playAreaWidth - targetSize * 2);
       targetY = Math.random() * (playAreaHeight - targetSize * 2);
-
-      console.log('Target Coordinates:', targetX, targetY);
-
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       ctx.beginPath();
       ctx.arc(targetX, targetY, targetSize, 0, 2 * Math.PI);
       ctx.fillStyle = 'red';
       ctx.fill();
       ctx.closePath();
-    } else {
-      console.log("Game not started or context not available");
     }
   };
 
@@ -73,19 +98,15 @@
     if (gameStarted && canvas) {
       const ctx = canvas.getContext('2d');
       const { offsetX, offsetY } = event;
-
-      const distance = Math.sqrt(
-        (offsetX - targetX) ** 2 + (offsetY - targetY) ** 2
-      );
-
-      totalClicks++; // Increment total clicks regardless of hitting the target or not
-
+      const distance = Math.sqrt((offsetX - targetX) ** 2 + (offsetY - targetY) ** 2);
+      totalClicks++;
       if (distance <= targetSize) {
         successfulClicks++;
         generateTarget(ctx);
       } else {
         missedClicks++;
       }
+      accuracy = (successfulClicks / totalClicks) * 100 || 0;
     }
   };
 
@@ -102,203 +123,162 @@
 
   const endGame = () => {
     gameEnded = true;
-    const ctx = canvas.getContext('2d');
-    ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
-    calculateStats();
-
-    // Show game controls when the game ends
-    const gameControlsContainer = document.querySelector('.game-controls-container');
-    gameControlsContainer.classList.remove('game-controls-hidden');
-    gameControlsContainer.classList.add('game-controls-visible');
-    resetGame(); // Reset the game state
-};
-const calculateStats = () => {
-    const totalClicksCount = successfulClicks + missedClicks;
-    const accuracy = totalClicksCount > 0 ? (successfulClicks / totalClicksCount) * 100 : 0;
-    const modal = document.getElementById('game-modal');
-    modal.style.display = 'block';
-    modal.innerHTML = `
-      <div class="modal">
-        <div class="modal-content" style="color: white;">
-          <h2>Game Stats</h2>
-          <p style="color: white;">Successful Clicks: ${successfulClicks}</p>
-          <p style="color: white;">Total Clicks: ${totalClicksCount}</p>
-          <p style="color: white;">Accuracy: ${accuracy.toFixed(2)}%</p>
-          <button id="play-again-button" style="background-color: #4caf50; color: white; padding: 10px 20px; font-size: 16px; border: none; border-radius: 5px; cursor: pointer; transition: background-color 0.3s ease;">Play Again</button>
-        </div>
-      </div>`;
-
-    // Add event listener separately
-    const playAgainButton = document.getElementById('play-again-button');
-    playAgainButton.addEventListener('click', startGame);
-
-    // Show game controls if the game has ended
-    const gameControlsContainer = document.querySelector('.game-controls-container');
-    gameControlsContainer.classList.remove('game-controls-hidden');
-    gameControlsContainer.classList.add('game-controls-visible');
-};
-
-  // Function to reset game state
-  const resetGame = () => {
-    gameStarted = false;
-    gameEnded = false;
+    gameStarted = false; // Reset game state
     successfulClicks = 0;
     missedClicks = 0;
     totalClicks = 0;
-    clickTimes = [];
-    timer = roundLength; // Reset round length
-    lastClickTime = null;
-    const gameControlsContainer = document.querySelector('.game-controls-container');
-    gameControlsContainer.classList.remove('game-controls-hidden');
-    gameControlsContainer.classList.add('game-controls-visible');
-};
+    accuracy = 0;
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    calculateStats();
+    showSettings = true; // Show settings popup after game ends
+  };
 
+  const calculateStats = () => {
+    showSettings = !showSettings; // Toggle settings panel visibility
+  };
 
+  let showSettings = false;
+
+  const toggleSettings = () => {
+    showSettings = !showSettings;
+  };
+
+  const applySettings = () => {
+    // Apply settings logic here
+    showSettings = false;
+  };
 </script>
-
-<head>
-  <title>Aim Trainer - Improve Your Shooting Accuracy!</title>
-  <meta name="description" content="Sharpen your aiming skills with this online aim trainer game. Practice your precision and speed by hitting targets as they appear on the screen. Challenge yourself and see how your accuracy improves over time!">
-  <meta name="keywords" content="aim trainer, shooting game, accuracy game, precision game, reflex game, target practice, online game, aim practice, shooting accuracy">
-  <meta name="author" content="Your Name">
-  <!-- Add more meta tags as needed -->
-</head>
-
-
-<Layout showThemeToggle={false}>
-  <div class="wrapper" role="main" aria-label="Aim Trainer Game">
-    <div class="game-controls-container" class:game-controls-visible={!gameEnded} class:game-controls-hidden={gameEnded}>
-      <h2>Game Controls</h2>
-      <label for="round-length">Round Length:</label>
-      <input type="range" id="round-length" min="5" max="30" bind:value={roundLength} />
-      <p>Round Length: {roundLength} seconds</p>
-      <label for="target-size">Target Size:</label>
-      <input type="range" id="target-size" min="10" max="50" bind:value={targetSize} />
-      <p>Target Size: {targetSize}px</p>
-    </div>
-    {#if !gameStarted}
-      <div class="start-game-modal">
-        <button on:click={startGame}>Start Game</button>
+<Layout>
+  <div class="wrapper">
+    <div class="settings-container {gameStarted ? 'settings-hidden' : ''}">
+      <div class="game-controls-container game-controls-visible">
+        <label for="target-size">Target Size:</label>
+        <input type="range" id="target-size" min="10" max="50" bind:value={targetSize}>
+        <label for="round-length">Round Length:</label>
+        <input type="range" id="round-length" min="5" max="60" bind:value={roundLength}>
+        <p>Game Time Left: {timer}s</p>
+        <button class="start-game-button" on:click={startGame}>Start Game</button>
       </div>
-    {:else}
-      <canvas bind:this={canvas} id="play-area" class="play-area" width={playAreaWidth} height={playAreaHeight} on:click={handleTargetClick}></canvas>
-    {/if}
-    <div id="game-modal" class="modal" style="display: none;"></div>
+    </div>
+    <canvas id="play-area" class="play-area" on:click={handleTargetClick}></canvas>
+    <div id="game-modal" class="modal">
+      <div class="modal-content">
+        <div class="settings-popup {showSettings ? 'settings-visible' : ''}">
+          <h2>Game Stats</h2>
+          <p>Successful Clicks: {successfulClicks}</p>
+          <p>Total Clicks: {totalClicks}</p>
+          <p>Accuracy: {accuracy.toFixed(2)}%</p>
+          <label for="modal-target-size">Target Size:</label>
+          <input type="range" id="modal-target-size" min="10" max="50" bind:value={targetSize}>
+          <label for="modal-round-length">Round Length:</label>
+          <input type="range" id="modal-round-length" min="5" max="60" bind:value={roundLength}>
+          <button class="apply-settings-button" on:click={applySettings}>Apply Settings</button>
+        </div>
+      </div>
+    </div>
   </div>
-  
 </Layout>
 
-
 <style>
+  .settings-popup {
+    display: none;
+    position: absolute;
+    top: 50px;
+    left: 50%;
+    transform: translateX(-50%);
+    z-index: 2;
+    background-color: rgba(0, 0, 0, 0.8);
+    color: #fff;
+    border-radius: 5px;
+    padding: 10px;
+  }
 
+  .settings-popup.settings-visible {
+    display: block;
+  }
 
   .wrapper {
+    position: relative;
     display: flex;
     justify-content: center;
     align-items: center;
     height: 100vh;
-    position: relative;
-    background-color: #1a1a1a; /* Dark background color */
-    color: #fff; /* Light text color */
+    background-color: #1a1a1a;
+    color: #fff;
+  }
+
+  .settings-container {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 2; /* Ensure settings container is on top */
+    background-color: rgba(0, 0, 0, 0.8); /* Adjust the background color and opacity as needed */
+    color: #fff;
+    border-radius: 5px;
+    padding: 10px;
+  }
+
+  .settings-hidden {
+    display: none;
   }
 
   .play-area {
     border: 2px solid white;
-    margin-top: 20px;
-    margin-left: -40px;
+    cursor: pointer;
+    width: 100%;
+    height: 100%;
+    max-width: 1000px;
+    min-width: 365px;
+    max-height: 75vh; /* Adjusted height */
+    position: relative;
   }
 
-  .start-game-modal {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    margin-top: 50px; /* Add some top margin to separate from the center */
-  }
-
-  .modal {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    background-color: #1a1a1a; /* Dark background color */
-    border-radius: 10px;
-    padding: 20px;
-    box-shadow: 0 0 10px rgba(255, 255, 255, 0.3); /* Lighter shadow color */
-    text-align: center;
-    color: #fff; /* Light text color */
-  }
-
-  .game-controls-visible {
-    display: block;
-    transform: translateY(-50%);
-    background-color: #2c2c2c; /* Darker background color */
-    padding: 20px;
-    border-radius: 10px;
-    box-shadow: 0 0 10px rgba(255, 255, 255, 0.3); /* Lighter shadow color */
-    color: #fff; /* Light text color */
-  }
-
-  .game-controls-hidden {
-    display: none;
-  }
-  
-  .start-game-modal button {
+  .start-game-button {
     padding: 10px 20px;
     font-size: 16px;
     border: none;
-    background-color: #4caf50; /* Green color for button */
-    color: #fff; /* Light text color */
+    background-color: #4caf50; /* Green color */
+    color: #fff;
+    cursor: pointer;
+    border-radius: 5px;
+    transition: background-color 0.3s ease;
+    margin-top: 10px;
+  }
+
+  .start-game-button:hover {
+    background-color: #45a049;
+  }
+
+  .modal {
+    display: none;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: rgba(0, 0, 0, 0.8);
+    color: white;
+    padding: 20px;
+    border-radius: 10px;
+  }
+
+  .modal-content {
+    text-align: center;
+  }
+
+  .play-again-button {
+    padding: 10px 20px;
+    font-size: 16px;
+    border: none;
+    background-color: #4caf50; /* Green color */
+    color: #fff;
     cursor: pointer;
     border-radius: 5px;
     transition: background-color 0.3s ease;
   }
 
-  .start-game-modal button:hover {
-    background-color: #45a049; /* Darker green color on hover */
-  }
-
-  /* Additional styling for game settings */
-  .game-controls-container {
-    position: absolute;
-    top: 50%;
-    right: 50%;
-    transform: translateY(-50%);
-    margin-right: 20%; /* Adjust as needed for separation */
-  }
-  .game-controls-container label {
-    font-weight: bold;
-    margin-bottom: 10px;
-    display: block;
-  }
-
-  .game-controls-container input[type="range"] {
-    width: 100%;
-    margin-bottom: 10px;
-    background-color: #555; /* Darker background color for range input */
-    border: none;
-    border-radius: 5px;
-    -webkit-appearance: none; /* Remove default appearance */
-  }
-
-  .game-controls-container input[type="range"]:focus {
-    outline: none; /* Remove focus outline */
-  }
-
-  .game-controls-container input[type="range"]::-webkit-slider-thumb {
-    -webkit-appearance: none; /* Remove default appearance */
-    width: 15px;
-    height: 15px;
-    background-color: #4caf50; /* Green color for thumb */
-    border-radius: 50%; /* Rounded shape */
-    cursor: pointer;
-  }
-
-  .game-controls-container p {
-    margin-bottom: 10px;
-  }
-
-  :focus {
-    outline: 2px solid #007bff; /* Example focus style */
+  .play-again-button:hover {
+    background-color: #45a049;
   }
 </style>
