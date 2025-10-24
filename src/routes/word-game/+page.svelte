fecef4ea51e11a174cbcebaf744c01b95b568381
BC1337 <brettcomrie@gmail.com>
2025-10-23 22:09:20 -0500

    added a huge word list client side to word game

--- src/routes/word-game/+page.svelte
+++ src/routes/word-game/+page.svelte
@@ -4,7 +4,112 @@
 
   const maxRows = 6;
   const wordLength = 5;
-  const words = ['apple', 'grape', 'melon', 'peach', 'berry', 'lemon'];
+
+const words = [
+  // A
+  'apple','ant','arm','axe','air','arrow','angel','apron','animal','arch','ask','add','able','also','after','any','age','act','ache','aim',
+  'ate','awake','answer','alone','around','alike','alive','angle','ask','allow','accept','attic','anchor','amuse','alert','apex','arise','abuse','agree','abound',
+
+  // B
+  'ball','bat','bear','bird','book','bread','bag','boat','bench','brick','bunny','berry','black','blue','bright','big','bad','back','bath','box',
+  'boy','bite','bake','bring','burn','bubble','broom','brush','bite','beach','beard','best','bite','brain','blend','break','brave','blind','bell','branch','build',
+
+  // C
+  'cat','cow','car','cup','cake','cloud','chair','coin','coat','crow','corn','cold','cute','clown','child','circle','clap','clean','cry','cook',
+  'crab','climb','cage','clue','catch','clerk','chalk','cheese','chief','chain','chess','chip','chain','clap','cling','coat','clash','crawl','curly','cross','cave',
+
+  // D
+  'dog','duck','door','drum','desk','dance','dust','doll','dark','dirt','day','deep','dream','dress','drop','dig','draw','deal','done','drive',
+  'dime','duckling','dare','dive','dull','drain','drip','dusty','damp','distant','dizzy','delight','dodge','drift','dwell','dine','dash','doodle','drain','dock','drumstick',
+
+  // E
+  'egg','ear','eye','eat','earth','echo','east','easy','edge','end','energy','enter','even','empty','early','each','eight','else','ever','exit',
+  'earn','emit','envy','evil','elbow','elk','echo','envoy','event','enjoy','entire','edit','erase','enter','equal','extra','enter','eager','exert','essay','engage',
+
+  // F
+  'fish','frog','fan','foot','face','farm','flag','fire','fruit','field','fast','fall','fun','full','first','friend','fly','fog','find','food',
+  'fence','frown','flame','fold','fancy','fetch','frame','fist','fade','fuel','foam','flee','flood','fright','feast','flash','flutter','flock','fringe','flare','ferry',
+
+  // G
+  'goat','girl','game','glass','grass','gate','gold','green','gum','giant','give','good','great','grow','ground','group','guess','glow','grab','gift',
+  'gaze','grin','grape','globe','grim','glide','gush','gravel','gallop','glance','gather','grimace','glisten','garden','guide','grain','greet','gear','goblin','ghost','grace',
+
+  // H
+  'hat','hand','horse','house','hill','heart','home','honey','hold','help','happy','high','hot','hard','hug','hear','heat','hunt','hope','head',
+  'hatch','hop','halt','hair','hollow','honest','hurry','hum','harm','heal','hero','hazel','hunt','hover','hatch','humble','howl','hub','habit','hike','hatch','hover','habit',
+
+  // I
+  'ice','ink','iron','island','idea','item','inch','inside','invite','ill','idle','icon','insect','iris','impact','imply','impose','intend','invest','income',
+  'ignite','input','idle','inch','ivory','invent','intact','interest','issue','itch','identity','isolate','image','immerse','impact','ignite','inspire','import','invite','index','idle','inlay','impact','inform',
+
+  // J
+  'jar','jug','jump','juice','jelly','joke','jeans','jungle','join','job','jolly','jet','judge','jog','jam','jewel','join','jazz','jump','joy',
+  'jest','jungle','jog','joint','jogging','jerk','jumping','juice','jiffy','jab','jack','jot','jacket','juggle','juncture','jubilant','justice','jingle','jersey','journey','jury','jolt','jargon','jewel',
+
+  // K
+  'kite','key','king','kick','kid','knee','knife','knot','kitten','know','kind','kept','knock','kitchen','kale','keen','karma','knob','kick','kiss',
+  'kayak','kettle','kingdom','knit','kink','kindle','kernel','kneel','kingly','knapsack','keep','kickoff','knockout','kooky','koala','kay','kiosk','knead','kooky','kudos','karma','knotty','kneel','kit','keen','kiddo',
+
+  // L
+  'lion','lamp','leaf','leg','lake','love','list','line','light','lamb','long','luck','last','late','look','lift','loop','loss','loud','learn',
+  'latch','leap','land','lazy','level','lean','lively','layer','link','listen','lurk','laugh','lick','lodge','lure','limit','load','lash','leave','let','load','loop','latch','lounge','lot','lift','light','lunar','lump','lock','luxury',
+
+  // M
+  'man','moon','milk','map','mouse','mango','mud','mask','meet','music','more','move','many','made','main','mine','menu','mean','mix','meal',
+  'mark','match','mend','mimic','maze','muddy','march','melt','moss','mop','mist','moth','mint','meat','mask','mock','mug','medal','myth','must','mirth','mile','mingle','mock','mute','moan','moist','march','munch','mirror',
+
+  // N
+  'net','nest','nose','nap','night','name','near','need','note','nail','news','near','nice','north','new','next','none','number','nature','novel',
+  'napkin','nick','nibble','narrow','nasty','neat','nudge','null','noun','nag','nerve','nickel','nifty','niche','notify','noise','noble','novice','nifty','nuzzle','nomad','nap','needle','nymph','noon','nape','note','nettle','node','nut',
+
+  // O
+  'owl','ox','ocean','orange','oil','open','old','odd','order','oven','once','only','over','own','outside','offer','object','okay','office','out',
+  'oak','ounce','orbit','omen','oxen','opinion','orbit','obey','oath','ogle','online','opt','open','omit','ounce','ostrich','outfit','outlet','overflow','outrun','overlay','overlap','offer','outrage','outline','outdo',
+
+  // P
+  'pig','pen','pan','pear','peach','plant','play','plate','pot','park','party','pink','page','pick','pull','push','point','pool','paint','pie',
+  'pond','pocket','patch','pound','palm','pause','peek','pitch','pile','poke','plow','plum','pray','peel','plug','plaza','prize','plain','prime','pride','plume','puff','paddle','paddle','pact','peer','pebble','peep','peanut','plenty','pat','paste','patrol',
+
+  // Q
+  'queen','quiz','quick','quiet','quack','quarter','question','quilt','quote','quality','quest','queue','quit','quirk','quill','quint','quake','quota','query','quark','quart',
+  'quarry','quench','quest','quibble','quintet','quota','quake','quaint','quasar','quill','quench','quarry','quirky','quiescent','quorum','quench','quip','quietly','quill','quid','quokka','quiver','quintuple','quorum','quizlet','quip','queue','quack','quince',
+
+  // R
+  'rat','run','rain','road','rock','rose','ring','river','rabbit','red','ride','rest','room','rule','right','rush','rough','reach','read','roll',
+  'rack','rust','race','ramp','rake','raven','rope','rail','rumble','riddle','rank','rant','roar','rack','reed','reap','reef','root','rely','risk','rivet','razor','rush','reign','rage','rail','rant','relax','robot',
+
+  // S
+  'sun','sky','sea','snake','star','sock','sand','shop','song','snow','slow','small','side','soft','stop','seat','study','sweet','safe','salt',
+  'soap','sack','seal','sink','sip','spin','spoon','spade','spike','spill','stack','stir','stand','step','stump','stamp','spit','sweep','spark','spend','streak','sling','spare','spring','spine','scene','slope','slide','slope','swim','spike','splash','sprout','squad','sack','swell','swirl','sneeze','snack','snail','snowy',
+
+  // T
+  'tree','tap','toy','top','tank','table','train','town','ten','time','take','talk','test','turn','tiny','tall','tire','trap','taste','team',
+  'task','trail','tame','tick','tug','twin','tank','toss','tide','tile','tread','trick','trophy','trunk','tune','twist','tuck','trend','thank','thorn','thump','thrive','trace','trail','tower','trade','thumb','treat','troll','toast',
+
+  // U
+  'urn','up','use','unit','under','uncle','until','urban','usual','unite','undo','urge','utter','update','upgrade','upset','uplift','uphold','umbrella','unique',
+  'upper','umpire','utilize','urgent','upbeat','utility','utterly','uproot','upstairs','untie','unfold','unpack','upland','unseen','undergo','upgrade','under','upend','uphold','undoing','underlay','unveil','underwear','unknown','unload','upstart','update','upvote','undercover','upkeep','unrest','underpass','upturn','union','underscore','understudy','unitary','unicorn','upwind','unwrap','unstable','untamed','uncurl','unclip',
+
+  // V
+  'van','vet','vine','vest','vase','voice','view','vivid','visit','value','vote','vocal','vast','valid','venture','victory','veil','vapor','verse','video',
+  'veil','vow','vent','vault','vibrate','venture','village','verse','vocal','venture','victim','victor','vigor','vane','volume','vacant','vivid','vacuum','vest','vanish','vocal','vantage','velvet','venture','vacate','visitor','vacuum','verdant','vividly','vestige','variant','vex','vortex','verbose','verify',
+
+  // W
+  'war','web','wax','wing','wolf','water','wind','wall','word','wave','wish','walk','wake','warm','watch','week','wide','wild','work','wise',
+  'wink','wander','wade','weigh','wool','wilt','woven','wobble','wander','warp','wring','wrest','whale','whip','whisk','whirl','wink','wrench','whack','wobble','wilt','whine','wax','wobble','wade','worry','weave','wobble','wobble','win','won','wipe',
+
+  // X
+  'xray','xylophone','xenon','xerox','xenial','xpress','xmas','xylem','xtra','xenial',
+  'xenon','xiphoid','xenial','xylitol','xenon','xerox','xmas','xray','xylitol','xenon','xenial','xeric','xyst','xenolith','xenophile','xerarch','xerophyte','xylocarp','xylograph','xenograft','xenocryst','xenolith','xylophone','xenon','xylotomy','xerox','xylidine','xenophile',
+
+  // Y
+  'yak','yard','yarn','yell','yellow','young','yes','yawn','year','yield','yoga','yolk','youth','yummy','yacht','yodel','yesterday','yearn','yeast','your',
+  'yearly','yonder','yell','yap','yip','yikes','yelp','yummy','yowl','yogurt','yarn','yap','youthful','yoke','yodeler','yellowish','yielding','yawner','yuppie','yikes','yuck','youth','yoga','yardstick','yaw','yarn','yo-yo','yelp','yoke','yearn','yummy','yellowy','yap','yowl','yip','yawn','yank','yacht','yowl','yammer','yes-man','yeoman',
+
+  // Z
+  'zoo','zip','zero','zone','zigzag','zebra','zen','zinc','zoom','zeal','zephyr','zest','zig','zap','zenith','zodiac','zigzag','zillion','zipper','zany',
+  'zest','zebra','zealot','zing','ziggy','zapper','zonal','zebrawood','zigzag','zinger','zeppelin','zoon','zodiacal','zoology','zoetic','zappy','zenana','zebu','zigzagged','zenithal','zealot','zealously','zeroed','zoster','zodiacally','zig','zip','zillionaire','zone','zoetrope','zygote'
+];
 
   let targetWord = '';
   let currentRow = 0;
